21e9be9dd5574e440a00c9cc1f6a8f42e68cc141
DayTroy <shadowyguy5@gmail.com>
2023-08-04 19:28:36 +0300

    Edit note implemented

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,21 +1,59 @@
-import { Link } from "react-router-dom"
-import {IoIosArrowBack} from "react-icons/io"
-import {RiDeleteBin6Line} from "react-icons/ri"
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { useState } from "react";
+import useCreateDate from "../hooks/useCreateDate";
 
-const EditNote = () => {
+const EditNote = ({ notes, setNotes }) => {
+  const { id } = useParams();
+  const note = notes.find((item) => item.id === id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+  const date = useCreateDate();
+  const navigate = useNavigate();
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      const newNote = {...note, title, details, date}
+      const newNotes = notes.map(note => {
+        if (note.id === id) {
+          note = newNote;
+        }
+        return note
+      })
+      setNotes(newNotes)
+    }
+    navigate("/");
+  }
+
   return (
     <section>
       <header className="create-note__header">
-        <Link to="/" className="button"><IoIosArrowBack /></Link>
-        <button className="button lg primary">Save</button>
-        <button className="button danger"><RiDeleteBin6Line/></button>
+        <Link to="/" className="button">
+          <IoIosArrowBack />
+        </Link>
+        <button className="button lg primary" onClick={handleForm}>Save</button>
+        <button className="button danger">
+          <RiDeleteBin6Line />
+        </button>
       </header>
       <form className="create-note__form">
-        <input type="text" placeholder="Title" autoFocus />
-        <textarea rows="28" placeholder="Note details..."></textarea>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+        />
+        <textarea
+          rows="28"
+          placeholder="Note details..."
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        ></textarea>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default EditNote
+export default EditNote;
